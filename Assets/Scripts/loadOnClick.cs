3e36ddd7b6c9d4a2b6def6ad565a55a37adfd892
Eric <EricTheDevil@hotmail.com>
2019-10-22 03:11:53 +0300

    Scene selector and Ngiah his changes

---
+++ Assets/Scripts/loadOnClick.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class loadOnClick : MonoBehaviour
+{
+    public GameObject loadingImage;
+    // Start is called before the first frame update
+    public void LoadScene(int level)
+    {
+        
+        SceneManager.LoadScene(level);
+    }
+}
