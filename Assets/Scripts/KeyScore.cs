e9d9cdbdc6d9817890597cab6fce333cfc317e21
Stefanos Edvard Kapsoritakis <stefan7@hotmail.gr>
2019-10-28 15:13:45 +0200

    Collection system

---
+++ Assets/Scripts/KeyScore.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KeyScore : MonoBehaviour
+{
+    Text text;
+    public static int keyAmount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        text.text = keyAmount.ToString();
+    }
+
+}
---
+++ Assets/Scripts/KillZoneScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillZoneScript : MonoBehaviour
+{
+    public Transform spawnPoint;
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.CompareTag("Player"))
+            collision.transform.position = spawnPoint.position;
+    }
+}
