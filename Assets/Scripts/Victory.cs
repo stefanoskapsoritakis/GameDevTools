958e3973eb2fa0c76ac21fb99893a0ba58f53134
Eric <EricTheDevil@hotmail.com>
2019-10-30 00:22:07 +0200

    Victory and loadingscreen

---
+++ Assets/Scripts/Victory.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Victory : MonoBehaviour
+{
+    public GameObject uiObject;
+    public Text score;
+    public int level;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        uiObject.SetActive(false);
+    }
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        var p = collider.gameObject.GetComponent<CharacterController>();
+
+        /*
+         if(score.text == "1")
+         {
+
+            uiObject.SetActive(true);
+            StartCoroutine(WaitForSec());
+         }
+        */
+        uiObject.SetActive(true);
+        StartCoroutine(WaitForSec());
+
+
+        IEnumerator WaitForSec()
+        {
+            yield return new WaitForSeconds(5);
+            SceneManager.LoadScene(level);
+            Destroy(uiObject);
+            Destroy(gameObject);
+        }
+    }
+}
