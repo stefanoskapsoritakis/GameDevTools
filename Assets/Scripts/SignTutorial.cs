55b576b3bbdc4dbe26f58d3abbdfd284b91e8f27
Eric <EricTheDevil@hotmail.com>
2019-10-30 22:03:45 +0200

    sign

--- Assets/Scripts/SignTutorial.cs
+++ Assets/Scripts/SignTutorial.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class SignTutorial : MonoBehaviour
 {
-
-
-    private void OnCollisionEnter2D(Collision2D collision)
+    public GameObject uiObject;
+    // Start is called before the first frame update
+    private void Start()
     {
-        
+        uiObject.SetActive(false);
+    }
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        var p = collider.gameObject.GetComponent<CharacterController>();
+        uiObject.SetActive(true);
+    }
+    void OnTriggerExit2D(Collider2D collider)
+    {
+        uiObject.SetActive(false);
     }
 }
