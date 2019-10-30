547e560fbc8da1d3c2e37ac72cd4aa2c3383b2cb
Stefanos Edvard Kapsoritakis <stefan7@hotmail.gr>
2019-10-30 19:53:39 +0200

    sign and sign code

---
+++ Assets/Scripts/SignTutorial.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SignTutorial : MonoBehaviour
+{
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+    }
+}
