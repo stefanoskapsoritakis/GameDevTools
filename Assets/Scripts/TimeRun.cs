e82a02527eb5938a885bf6a17505330ea3e88321
Stefanos Edvard Kapsoritakis <stefan7@hotmail.gr>
2019-11-16 18:53:41 +0200

    2nd level almost finished, 1st level sky fix, started 3rd level

---
+++ Assets/Scripts/TimeRun.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeRun : MonoBehaviour
+{
+    public Text timeText;
+    public float time = 30;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateTime();
+    }
+    public void UpdateTime()
+    {
+        time = time - Time.deltaTime;
+        timeText.text = "Time: " + Mathf.Round(time);
+    }
+}
---
+++ Assets/Scripts/TreatScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TreatScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public TimeRun timeRun;
+
+    void Start()
+    {
+        timeRun = FindObjectOfType<TimeRun>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        timeRun.time += 5;
+        timeRun.UpdateTime();
+        Destroy(gameObject);
+    }
+}
