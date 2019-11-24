744976a3e2729c0060706c0998724b158f3c2609
Eric <EricTheDevil@hotmail.com>
2019-11-24 23:58:39 +0200

    Yeho

---
+++ Assets/Scripts/IdleWalker.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IdleWalker : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject dog;
+
+    private float runSpeed = 5f;
+
+    bool jump = false;
+    Vector3 temp = new Vector3(-25.91f, -16.18f, -3f);
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate((Vector3.right * runSpeed) * Time.fixedDeltaTime );
+        if(dog.transform.position.x > 25)
+        {
+            dog.transform.position = temp;
+
+        }
+
+
+    }
+
+}
