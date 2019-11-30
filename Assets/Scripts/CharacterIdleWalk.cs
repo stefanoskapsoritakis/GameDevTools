561b185b4dbddcceb7cc42074a7c9e2d019eadf8
Stefanos Edvard Kapsoritakis <stefan7@hotmail.gr>
2019-11-30 20:05:06 +0200

    Last scene finished, Credits addded and finished (missing sounds)

---
+++ Assets/Scripts/CharacterIdleWalk.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterIdleWalk : MonoBehaviour
+{
+    public float speed = 5f;
+    public Animator idleAnimator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.right * Time.fixedDeltaTime * speed);
+        idleAnimator.SetFloat("Speed", Mathf.Abs(speed));
+    }
+}
---
+++ Assets/Scripts/GirlCollision.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GirlCollision : MonoBehaviour
+{
+    public GameObject canvas;
+    public GameObject scene;
+    public int sceneNum;
+    public GameObject Girl;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        canvas.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        canvas.SetActive(true);
+        scene.SetActive(false);
+        GameObject.Find("Girl").transform.localScale = new Vector3(0, 0, 5);
+        StartCoroutine(WaitForSec());
+
+        IEnumerator WaitForSec()
+        {
+            yield return new WaitForSeconds(5);
+            SceneManager.LoadScene(sceneNum);
+        }
+    }
+}
