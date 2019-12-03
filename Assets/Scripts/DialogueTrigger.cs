14f652fdb2621df53f00e41a4e29644bd6eb8aa1
Eric <EricTheDevil@hotmail.com>
2019-12-03 19:35:15 +0200

    fixed Camera on Girlpicture, made it so the dialogue vanishes after x seconds

--- Assets/Scripts/DialogueTrigger.cs
+++ Assets/Scripts/DialogueTrigger.cs
@@ -12,6 +12,17 @@ public class DialogueTrigger : MonoBehaviour
         var p = collider.gameObject.GetComponent<CharacterController>();
         TriggerDialogue();
     }
+    void OnTriggerExit2D(Collider2D other)
+    {
+        StartCoroutine(ToFar());
+        
+    }
+    IEnumerator ToFar()
+    {
+        yield return new WaitForSeconds(3F);
+        FindObjectOfType<DialogueSystem>().EndDialogue();  
+   
+    }
     public void TriggerDialogue()
     {
         FindObjectOfType<DialogueSystem>().StartDialogue(dialogue);
